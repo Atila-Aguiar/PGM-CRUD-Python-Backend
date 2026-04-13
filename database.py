bcd5f93def2641494d66c7c8408b62decaedde78
Atila_Aguiar <atilasilva007@gmail.com>
2026-04-13 15:02:17 -0300

    Adicionando alterações para subir pra nuvem no railway

--- database.py
+++ database.py
@@ -1,17 +1,15 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./tarefas.db"
+os.makedirs("/app/data", exist_ok=True)
+
+DATABASE_URL = "sqlite:////app/data/tarefas.db"
 
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
